7c6baa9dde9072b3a73511d872c30d0c3f136540
tiger <xiahaihu213@163.com>
2019-03-02 11:20:37 +0800

    chapter 12.5

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,34 @@
-use std::io;
-use std::env;
-use std::fs;
 use std::cmp::Ordering;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::io;
+use std::process;
 use rand::Rng;
-
-mod sound {
-    pub mod instrument {
-        pub fn clarinet() {
-            // Function body code goes here
-        }
-    }
-}
-
-mod performance_group {
-    pub use crate::sound::instrument;
-
-    pub fn clarinet_trio() {
-        instrument::clarinet();
-        instrument::clarinet();
-        instrument::clarinet();
-    }
-}
+use rust_hello::Config;
+use rust_hello;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = rust_hello::run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
 fn a() {
-     performance_group::clarinet_trio();
-     performance_group::instrument::clarinet();
+    performance_group::clarinet_trio();
+    performance_group::instrument::clarinet();
 }
 
 fn guess_game() {
@@ -66,7 +55,25 @@ fn guess_game() {
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            },
+            }
         }
+    }
+}
+
+mod sound {
+    pub mod instrument {
+        pub fn clarinet() {
+            // Function body code goes here
+        }
+    }
+}
+
+mod performance_group {
+    pub use crate::sound::instrument;
+
+    pub fn clarinet_trio() {
+        instrument::clarinet();
+        instrument::clarinet();
+        instrument::clarinet();
     }
 }
